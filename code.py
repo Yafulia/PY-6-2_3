249dffa68168f1ef8a13a168e667ebc050f0a1f7
Yafa Yusufova <yafulia@mail.ru>
2017-07-24 16:49:38 +0300

    Стилистические ошибки исправлены.

--- code.py
+++ code.py
@@ -23,7 +23,7 @@ def print_top_10(f):
 		words.extend(description)
 		words.extend(title)
 	
-	words_freq = [(words.count(word), word) for word in set (words) if len(word) > 6]
+	words_freq = [(words.count(word), word) for word in set(words) if len(word) > 6]
 	words_freq.sort(reverse=True)
 	for word_freq in words_freq[:10]:
 		print(word[1])
